734abb2d839032e0a87ce3fb0ff61e9c99fc84f5
akash <a@b.com>
2025-01-13 13:30:29 +0530

    myangel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('naya he wah ');
+ res.send('ho jayega naya jaga pe ');
 });//listen to port 5000 by default
 app.listen(process.env.PORT || 5000);
  
