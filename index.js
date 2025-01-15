1ca973ebdd837a0e4ec13f15ecdeb7faa83d8864
akash <a@b.com>
2025-01-15 13:15:18 +0530

    hojayega

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('ho jayega naya jaga pe ');
+ res.send('hello world ');
 });//listen to port 5000 by default
 app.listen(process.env.PORT || 5000);
  
