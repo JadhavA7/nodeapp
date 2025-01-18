3402007de952f85f96710867589b47f6cbb0bd4e
akash <a@b.com>
2025-01-18 20:42:05 +0530

    hogakya

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var app = express();//Respond with "hello world" for requests that hit our root 
 app.get('/', function (req, res) {
  res.send('hello world');
 });//listen to port 5000 by default
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000);
  
 module.exports = app;
