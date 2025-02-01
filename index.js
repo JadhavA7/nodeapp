05b34ec71d2c297a16a3f8f4881dce8f221c6705
akash <a@b.com>
2025-02-01 20:18:57 +0530

    ho

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hoga kya ');
+ res.send('ho gaya ');
 });//listen to port 5000 by default
 app.listen(process.env.PORT || 3000);
  
